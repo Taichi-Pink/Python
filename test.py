eeece64ddc201471b7712f828e64306d2f8fe3db
Taichi <15868881759@163.com>
2021-12-20 12:38:14 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('/Volumes/My Passport/wdrface/WDR_face_dataset/WDR_tiff/scene1/01/Scene1_One-Face0.tif', flags=cv2.IMREAD_ANYDEPTH)
+img = cv2.imread('./wdrface/WDR_face_dataset/WDR_tiff/scene1/01/Scene1_One-Face0.tif', flags=cv2.IMREAD_ANYDEPTH)
 print(np.min(img), np.max(img))
 img = np.expand_dims(img, axis=2)
 img = np.concatenate([img, img, img], axis=2)
